0b06af42913bab408e9b7807add9ddc4ad1fcfa7
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-03-17 01:19:30 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Paths from './components/routs/Paths';
+import Sidebar from './components/sidebar/Sidebar';
+import { Flex,Center } from '@chakra-ui/react';
 function App() {
   return (
     <div className="App">
-     <Paths/>
+      <Flex gap={20}>
+      <Sidebar/>
+      <Center>
+      < Paths/>
+      </Center>
+       
+      </Flex>
+     
     </div>
   );
 }
