8571236b1fd194b9b7ea827dceae39aaedb47429
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-03-17 00:50:29 +0530

    Create NavItem.js

---
+++ client/src/components/sidebar/NavItem.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+    Flex,
+    Text,
+    Icon,
+    Link,
+    Menu,
+    MenuButton,
+    MenuList
+} from '@chakra-ui/react'
+import NavHoverBox from './NavHoverBox'
+
+export default function NavItem({ icon, title, description, active, navSize }) {
+    return (
+        <Flex
+            mt={30}
+            flexDir="column"
+            w="100%"
+        borderRadius="30px"
+            sx={{
+        background:
+          'linear-gradient( #fff 0 0) padding-box, linear-gradient(90deg, #0085AB 0%, #2CB2C3 34.2%, #F9D520 69.77%, #D1D922 102.6%) border-box',
+        border: '3px solid transparent',
+      
+
+      }}
+            alignItems={navSize == "small" ? "center" : "flex-start"}
+        >
+            <Menu placement="right">
+                <Link
+                    backgroundColor={active && "#AEC8CA"}
+                    p={3}
+                    borderRadius="30px"
+                    _hover={{ textDecor: 'none', backgroundColor: "#AEC8CA" }}
+                    w={navSize == "large" && "100%"}
+                >
+                    <MenuButton w="100%">
+                        <Flex>
+                            <Icon as={icon} fontSize="xl" color={active ? "#82AAAD" : "gray.500"} />
+                            <Text ml={5} display={navSize == "small" ? "none" : "flex"}>{title}</Text>
+                        </Flex>
+                    </MenuButton>
+                </Link>
+                <MenuList
+                    py={0}
+                    border="none"
+                    w={200}
+                    h={200}
+                    ml={5}
+                >
+                    <NavHoverBox title={title} icon={icon} description={description} />
+                </MenuList>
+            </Menu>
+        </Flex>
+    )
+}
