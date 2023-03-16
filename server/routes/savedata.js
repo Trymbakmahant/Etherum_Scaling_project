9854405931da1242554cdfae4c52debbd3f2f409
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-03-16 18:39:57 +0530

    db work

--- server/routes/savedata.js
+++ server/routes/savedata.js
@@ -2,13 +2,26 @@ const router = require("express").Router();
 import { Polybase } from "@polybase/client";
 const db = new Polybase({ defaultNamespace: "your-namespace" });
 
-await db.collection("City").create(["new-york", "New York"]);
-
 router.route("/").post(async (req, res) => {
   try {
+    // your-namespace is optional if you have defined a default namespace
+
+    await db.collection("City").create(["ney-york", "Ney York"]);
   } catch (e) {
     console.log(e);
   }
+});
+
+router.route("/update").post(async (req, res) => {
+  try {
+    await db.collection("City").call("setCountry", ["USA"]);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.route("./read").post(async (req, res) => {
+  const data = await db.collection("City").record("new-york").get();
 });
 
 module.exports = router;
