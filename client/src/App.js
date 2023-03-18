ba8501245fcf2e2da9220d765e4207eed3c72ca0
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-03-18 14:51:11 +0530

    basic orientation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Paths from "./components/routs/Paths";
+import Paths from "./components/Routes/path";
 import Sidebar from "./components/sidebar/Sidebar";
 import { Flex, Center } from "@chakra-ui/react";
 function App() {
--- client/src/components/Routes/path.js
+++ client/src/components/Routes/path.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
-import Upload from "../Pages/Polls";
+import Polls from "../Pages/login";
 
 function Path() {
   return (
     <Routes>
-      <Route exact path="/" element={<Upload />} />
+      <Route exact path="/" element={<Polls />} />
     </Routes>
   );
 }
