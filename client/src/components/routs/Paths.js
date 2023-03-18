0c1af92f42c45a2edf2f41f32aebbe22ba40db9d
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-03-18 22:14:41 +0530

    Update Paths.js

--- client/src/components/routs/Paths.js
+++ client/src/components/routs/Paths.js
@@ -1,11 +1,17 @@
 import {Routes,Route} from "react-router-dom";
+import Pols from "../card/Pols";
+import Polscard from "../card/Polscard";
 import PostCard from "../card/Postcard";
+import Login from "../login/Login";
 const Paths=()=>{
     return(
 
 
     <Routes>
         <Route exact path="/" element= {<PostCard/>}/>
+        <Route exact path="/polls" element ={<Polscard/> }/>
+        <Route exact path="/pols" element ={<Pols/> }/>
+        <Route exact path="/login" element ={<Login/> }/>
     </Routes>
         )
 }
