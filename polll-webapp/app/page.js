b739ad802b6841e414d4d2ab3b1049fcfd6b6ac1
Ajay krishnanand Paikra <75928188+Ajay12-p@users.noreply.github.com>
2023-03-20 21:17:06 +0530

    Update page.js

--- polll-webapp/app/page.js
+++ polll-webapp/app/page.js
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import { Rubik_80s_Fade } from "next/font/google";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const fonta = Rubik_80s_Fade({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
-  return (
+
+  return (<>
+ 
+  
     <main className={styles.main}>
       <div className={styles.txt}>
         <p className={fonta.className}>Polll</p>
+      <Link href="/polls">
+        click
+       </Link>
       </div>
     </main>
+    </>
   );
 }
