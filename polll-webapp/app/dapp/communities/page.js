9ac0fea52c553bf919b33cc04edbfdb96f46de76
vanoob404 <vanoobcoder@gmail.com>
2023-03-22 19:34:37 +0530

    small changes

--- polll-webapp/app/dapp/communities/page.js
+++ polll-webapp/app/dapp/communities/page.js
@@ -4,8 +4,7 @@ import { useEffect, useState, useRef } from "react";
 import { Polybase } from "@polybase/client";
 const page = () => {
   const [data, setdata] = useState([]);
-
-  var alldata = [];
+  const [isDataLoaded, setDataLoaded] = useState(false);
   const db = new Polybase({
     defaultNamespace:
       "pk/0xbbc1ff78605c9f8c178d474e3d66aca2512b2d59838fac927f23320f5b101fca1b7ed14f387e0ff09a0ded2f6468be24f87e23328a472e7692ae25dae8d4f120/PRACTICE",
@@ -15,24 +14,31 @@ const page = () => {
 
   async function GetRecord() {
     const records = await Communities.get();
-    console.log(records.data[0].data);
+    // console.log(records.data[0].data);
     setdata(records.data);
-
-    console.log(data);
   }
-
   useEffect(() => {
     GetRecord();
   }, []);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      setDataLoaded(true);
+      console.log(data);
+    }
+  }, [data]);
   return (
     <div className={style.wraper}>
-      {/* {console.log("consaole.llgohfiwoehjwo")}
-      {alldata.length > 0 &&
-        alldata.map((post) => {
-          console.log("hello bhai sahab");
-          return <p> {post.data.name}</p>;
-        })} */}
-      <p>{data && data[0].data}</p>
+      <div className={style.card}>
+        {isDataLoaded &&
+          data.map((item) => {
+            return (
+              <div key={item.data.id}>
+                <p>{item.data.name}</p>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
--- polll-webapp/app/dapp/components/sidebar.js
+++ polll-webapp/app/dapp/components/sidebar.js
@@ -55,6 +55,20 @@ export default function Sidebar() {
           >
             <Link href={"/dapp/user"}>User</Link>
           </motion.div>
+          <motion.div
+            initial={{ y: "100%", opacity: 0 }}
+            animate={{ y: "0", opacity: 1, transition: { delay: 1.4 } }}
+            transition={{ type: "spring", duration: 0.8 }}
+            whileHover={{
+              type: "spring",
+              x: 20,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            className={styles.navitem}
+          >
+            <Link href={"/dapp/communities"}>create community</Link>
+          </motion.div>
         </div>
       </motion.nav>
     </>
